816e343dc031831c003df48ae37a4fe399ec722c
LevanovaElena <ellevanova@gmail.com>
2020-10-17 20:06:24 +0400

    feat:add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = n.toString();
+    let rezult = '';
+    for (i = str.length - 1; i >= 0; i--) {
+        rezult = rezult + str[i];
+    }
+    return parseInt(rezult);
 }
